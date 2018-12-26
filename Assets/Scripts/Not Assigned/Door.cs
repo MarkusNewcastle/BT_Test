34828c83fbfce80757dae29b7773687af0c37e07
markusnewcastle <markusnewcastle@gmail.com>
2018-12-26 08:19:03 +0100

    sample BT

---
+++ Assets/Scripts/Not Assigned/Door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public Transform interActionPoint;
+
+
+    public void OpenDoor() {
+
+        gameObject.SetActive(false);
+    }
+}
